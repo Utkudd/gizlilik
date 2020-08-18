b7f62e42920e78d5eb06ca1ff12622512c025ef1
Utkudd <68447015+Utkudd@users.noreply.github.com>
2020-08-18 21:04:33 +0300

    gizlilik

---
+++ gizlilik.js
@@ -0,0 +1,11 @@
+client.on('voiceStateUpdate', async (___, newState) => {
+
+  if (
+    newState.member.user.bot &&
+    newState.channelID &&
+    newState.member.user.id == client.user.id &&
+    !newState.selfDeaf
+  ) {
+    newState.setSelfDeaf(true);
+  }
+});
